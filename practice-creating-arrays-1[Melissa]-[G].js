8b40150e6cc5adf410a0ffd0292247d35775e98a
Cuzenu <gendronm@prodigy.net>
2025-03-31 19:02:35 -0500

    Lesson 1 practice

--- practice-creating-arrays-1[Melissa]-[G].js
+++ practice-creating-arrays-1[Melissa]-[G].js
@@ -1,3 +1,14 @@
-console.log("hello");
+//let colors = ["red", "green", "blue"];
+// print the whole array
+//console.log(colors);
+
+// Create 3 arrays using array literals for the following: 
+
+let groceryList = ["apple", "banana", "milk", "eggs", "ice cream"];
+let groceryPrices = ["$2", "$6", "$10"];
 
 
+console.log("1. " + groceryList);   // log the entire groceryList array
+console.log("2. " + groceryPrices);   // log the list of gorceryPrices array
+console.log("3. " + groceryList[1] + ", " + groceryList[3] + ", " + groceryList[4]);    // log 3 different items
+console.log("4. " + groceryPrices[0] + ", " + groceryPrices[2]);
